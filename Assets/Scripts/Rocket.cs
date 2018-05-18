07a46a90a00596ad110fb188650e6bcbdf790190
Anthony Mendez <anthonymendez9@gmail.com>
2018-05-17 22:52:20 -0400

    S3:L47: Added Thrusting sound effect when thrusting. Applied Rocket changes to Rocket prefab.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -21,6 +23,10 @@ public class Rocket : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) {
             rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying)
+                audioSource.Play();
+        } else {
+            audioSource.Pause();
         }
         if (!(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))) {
             if (Input.GetKey(KeyCode.A)) {
